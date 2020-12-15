7b7c6034c17c9ea4e3f9318014327e5ba1ad7877
PGrover2001 <40870043+PGrover2001@users.noreply.github.com>
2020-12-15 23:47:57 +0000

    today's work, made the post component. Feeling low so will do it toms. minium efforts achieved

--- src/Post.js
+++ src/Post.js
@@ -1,9 +1,20 @@
+import { Avatar } from "@material-ui/core";
 import React from "react";
 import "./Post.css";
-function Post() {
+function Post({ username }) {
   return (
     <div className="post">
-      <h1>I am post</h1>
+      <h3>Username</h3>
+      <Avatar className="post__avatar" alt={username} />
+      <img
+        className="post__image"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/React_Girls_Berlin.png/673px-React_Girls_Berlin.png"
+        alt=""
+      />
+
+      <h4 className="post__text">
+        <strong>Hardeep:</strong> Day two of insta clone
+      </h4>
     </div>
   );
 }
